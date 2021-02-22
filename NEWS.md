6d85e042b25d690580c3da6db1153bfc8a7afc5a
Tobias Pilz <topilz@pik-potsdam.de>
2021-02-22 17:19:46 +0100

    ENH: Added package documentation

--- NEWS.md
+++ NEWS.md
@@ -3,6 +3,7 @@
 - added unit tests (so far only for `tin()`) and codecov integration
 - added `read_msh()`
 - `tin()` supports Gmsh mesh output (only format version 2) to infer TIN
+- added package documentation `?telemac`
 ## Fixes
 - `tin()`
   - check of curve orientation of outer boundary
--- R/telemac.R
+++ R/telemac.R
@@ -1,9 +1,13 @@
-#' @keywords internal
 #' @import rlang
 #' @importFrom magrittr "%>%"
 #' @importFrom utils write.table read.table tail
 #' @importFrom graphics plot
 #' @useDynLib telemac, .registration=TRUE
 #' @importFrom Rcpp sourceCpp
-
 globalVariables(".")
+
+#' @description This package includes methods for initialisation, simulation, and
+#' visualisation of the hydrodynamic model TELEMAC-2D. To start with the package
+#' first have a look at the vignettes: \code{browseVignettes(package = "telemac")}
+#' @keywords internal
+"_PACKAGE"
